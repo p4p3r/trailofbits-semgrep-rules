8c7ec587bc894cc54db8376ddb36dd61a47f5d45
Claudio <claudio@r2c.dev>
2022-05-23 17:23:16 +0200

    Reduce match length for waitgroup-add-called-inside-goroutine

--- go/waitgroup-add-called-inside-goroutine.go
+++ go/waitgroup-add-called-inside-goroutine.go
@@ -10,12 +10,30 @@ import (
 
 
 func main() {
-	// ruleid: waitgroup-add-called-inside-goroutine
 	var wg sync.WaitGroup
 	var x int32 = 0
 	for i := 0; i < 100; i++ {
 		go func() {
+	        // ruleid: waitgroup-add-called-inside-goroutine
 			wg.Add(1)
+			atomic.AddInt32(&x, 1)
+			wg.Done()
+		}()
+	}
+
+	fmt.Println("Wait ...")
+	wg.Wait()
+	fmt.Println(atomic.LoadInt32(&x))
+}
+
+
+func main() {
+	var wg sync.WaitGroup
+	var x int32 = 0
+	for i := 0; i < 100; i++ {
+	    // ok: waitgroup-add-called-inside-goroutine
+		wg.Add(1)
+		go func() {
 			atomic.AddInt32(&x, 1)
 			wg.Done()
 		}()
